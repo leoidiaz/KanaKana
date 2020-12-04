2176393274ff70f87268ef544d6edfaeebe778a1
leoidiaz <zaid.leonardo@gmail.com>
2020-12-03 18:16:40 -0800

    project init

---
+++ KanaKana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanaKana
+//
+//  Created by Leonardo Diaz on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
