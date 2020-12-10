4569ebf0b9d8d4851435e52abfeeb141ccc42aee
leoidiaz <zaid.leonardo@gmail.com>
2020-12-09 19:35:33 -0800

    create files for UIBUtotn Extension and UIStackView Extension

---
+++ KanaKana/Utility/UIButton+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+Extension.swift
+//  KanaKana
+//
+//  Created by Leonardo Diaz on 12/9/20.
+//
+
+import Foundation
---
+++ KanaKana/Utility/UIStackView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIStackView+Extension.swift
+//  KanaKana
+//
+//  Created by Leonardo Diaz on 12/9/20.
+//
+
+import Foundation
