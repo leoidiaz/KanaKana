6ebc1cf8976ddef53c796e74b4f2e278720f3dfe
leoidiaz <zaid.leonardo@gmail.com>
2020-12-09 19:36:55 -0800

    Move makeStackView into seperate file

--- KanaKana/Utility/UIStackView+Extension.swift
+++ KanaKana/Utility/UIStackView+Extension.swift
@@ -5,4 +5,15 @@
 //  Created by Leonardo Diaz on 12/9/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIStackView {
+    static func makeStackView(alignment: UIStackView.Alignment, distribution: UIStackView.Distribution, spacing: CGFloat = 0, axis: NSLayoutConstraint.Axis) -> UIStackView {
+        let stackView = UIStackView()
+        stackView.alignment = alignment
+        stackView.distribution = distribution
+        stackView.spacing = spacing
+        stackView.axis = axis
+        return stackView
+    }
+}
