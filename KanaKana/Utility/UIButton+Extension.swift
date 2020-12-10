716b86d8f1b1b2c048d7e4d8cfd05e719e579a41
leoidiaz <zaid.leonardo@gmail.com>
2020-12-09 19:36:27 -0800

    Move makeButton into seperate file

--- KanaKana/Utility/UIButton+Extension.swift
+++ KanaKana/Utility/UIButton+Extension.swift
@@ -5,4 +5,16 @@
 //  Created by Leonardo Diaz on 12/9/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton{
+    static func makeButton(text: String, color: UIColor, fontSize: CGFloat = UIFont.labelFontSize) -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle(text, for: .normal)
+        button.setTitleColor(color, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: fontSize)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        return button
+    }
+}
