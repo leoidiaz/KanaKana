cc098ae1eeb19820ac9051694b4d8f02178c69cc
leoidiaz <zaid.leonardo@gmail.com>
2020-12-09 19:32:54 -0800

    Refactor HiraganaManager

--- KanaKana/Managers/HiraganaManager.swift
+++ KanaKana/Managers/HiraganaManager.swift
@@ -7,35 +7,43 @@
 
 import Foundation
 
-class HiraganaManager {
-    var networkService = NetworkManager()
+class HiraganaManager: NetworkManager {
     static var shared = HiraganaManager()
     var allHira = [Hiragana]()
     var hira = [String:[Hiragana]]()
     func fetchHiragana(completion: @escaping(Result<[Type], KanaErrors>) -> Void){
-        networkService.fetchHiragana { [weak self] (result) in
+        guard let url = EndPoint.baseURL else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        let request = URLRequest(url: url)
+        perform(request) { [weak self] (result) in
             switch result {
             case .success(let data):
-                do {
-                    let hiragana = try JSONDecoder().decode([Hiragana].self, from: data)
-                    self?.allHira = hiragana
-                    let sectionHiragana = Dictionary(grouping: hiragana, by: { String($0.type)})
-                    self?.hira = sectionHiragana
-                    var types = [Type]()
-                    for i in sectionHiragana {
-                        let type = Type(type: i.key, hiragana: i.value)
-                        types.append(type)
-                    }
-                    let sortedTypes = types.sorted(by: {$0.order < $1.order})
-                    return completion(.success(sortedTypes))
-                } catch {
-                    print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
-                    return completion(.failure(.thrownError(error)))
-                }
+                self?.sortHira(data, completion: completion)
             case .failure(let error):
                 print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
-                return completion(.failure(.thrownError(error)))
+                completion(.failure(.thrownError(error)))
             }
         }
+    }
+}
+
+extension HiraganaManager {
+    private func sortHira(_ data: Data, completion:@escaping (Result<[Type], KanaErrors>)-> Void){
+        guard let hiragana = data.decode(type: [Hiragana].self) else {
+            completion(.failure(.unableToDecode))
+            return
+        }
+        allHira = hiragana
+        let sectionHiragana = Dictionary(grouping: hiragana, by: { String($0.type)})
+        hira = sectionHiragana
+        var types = [Type]()
+        for i in sectionHiragana {
+            let type = Type(type: i.key, hiragana: i.value)
+            types.append(type)
+        }
+        let sortedTypes = types.sorted(by: {$0.order < $1.order})
+        return completion(.success(sortedTypes))
     }
 }
