4bb6a6ea6256de6f26fcb770fbba53cf7a458d24
leoidiaz <zaid.leonardo@gmail.com>
2020-12-16 13:47:05 -0800

    UIColor+Extension

---
+++ KanaKana/Utility/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  KanaKana
+//
+//  Created by Leonardo Diaz on 12/16/20.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    static let gris = UIColor(named: "gris")!
+}
